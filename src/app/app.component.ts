5ad1f0336e89f09052d260afde13e68dcdf87588
derrrick william <williamderrick100@gmail.com>
2019-01-20 17:29:09 +0300

    Made new updates on app components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from 'src/app/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  users:string[];
   
-}
+  // users= [
+  //   new User(1,'Derrick')
+  // ]
+  }
+  
+
