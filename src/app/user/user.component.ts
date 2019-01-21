8aad4af34d8a47ad49ed76fe144d063a4eb36b59
derrrick william <williamderrick100@gmail.com>
2019-01-21 15:19:24 +0300

    Corrected Errors arising after editing user service.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../user';
+import {UserService} from 'src/app/users/user.service';
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+  providers:[UserService],
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
---
+++ src/app/users/user.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Users } from '../users';
+
+@Injectable()
+export class UserService {
+ 
+ }
+  constructor() { }
+}
